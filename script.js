e46c8bbc3c6544ccfb81e45141f75078a1eed82f
Elena Petrova <helenofspb@gmail.com>
2025-06-20 12:11:29 +0300

    Loan Function created

--- script.js
+++ script.js
@@ -172,3 +172,33 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount / 10)) {
+    //Add the movement
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  } else {
+    alert('Sorry, too big amount!');
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
